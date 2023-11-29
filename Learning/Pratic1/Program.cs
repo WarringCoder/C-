557a4396927d7e32331023a566fb148d105a5a51
İshak Eren <ishakerenn1905@gmail.com>
2023-11-29 17:34:03 +0300

    completed

---
+++ Learning/Pratic1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pratic1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Lütfen " + (i+1) + ". Sayıyı Giriniz: ");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+            int sonuc =0;
+
+            foreach (int a in sayilar)
+            {
+                sonuc += a;
+            }
+            Console.WriteLine("Girdiğiniz sayıların toplamı: " + sonuc);
+            Console.ReadLine();
+
+
+        }
+    }
+}
