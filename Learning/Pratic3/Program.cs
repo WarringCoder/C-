0b13cf9008c4fdb4797e9ddc3955a5b5a0d69d1e
İshak Eren <ishakerenn1905@gmail.com>
2024-01-03 12:16:17 +0300

    new pratic

---
+++ Learning/Pratic3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Pratic3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int Kac_Saat_Calisma= 0;
+            string Zihin_Kontrol;
+            string Atomik_Aliskanliklar;
+            string Esneme_Spor;
+            
+            Console.WriteLine("1-) Lütfen Günde Kaç Saat Çalışdığınızı Giriniz: ");
+            Kac_Saat_Calisma += int.Parse(Console.ReadLine());
+            Console.WriteLine("2-) Lütfen Her Gün Zihin Kontrol çalışmalarını yapıp yapmadığınızı belirtiniz (YES / NO): ");
+            Zihin_Kontrol = Console.ReadLine();
+            Console.WriteLine("3-) Lütfen Her Gün Atomik Alışkanlıklarınızı yapıp yapmadığınızı belirtiniz (YES / NO): ");
+            Atomik_Aliskanliklar = Console.ReadLine();
+            Console.WriteLine("4-) Lütfen Her Gün Esneme / Spor yapıp yapmadığınızı belirtiniz (YES / NO): ");
+            Esneme_Spor = Console.ReadLine();
+
+            if (Kac_Saat_Calisma > 7)
+            {
+                if (Zihin_Kontrol == "yes")
+                {
+                    if (Atomik_Aliskanliklar == "yes")
+                    {
+                        if (Esneme_Spor == "yes")
+                        {
+                            Console.WriteLine("TEBRİKLER :) Hergün Böyle Çalışmaya devam ederseniz 6 aya kalmaz YAZILIMCI olarak MÜKEMMEL bir İŞE girebilirsiniz :) ");   
+                        }
+                        
+                    }
+                }
+                
+            }
+            
+            
+            
+
+        }
+    }
+}
